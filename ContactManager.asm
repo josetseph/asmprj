d647f8d05d0798eff84fa1ec77c30e4b80c3c154
wrtnspknbrkn <55252590+Joseph-DKE@users.noreply.github.com>
2021-07-05 14:23:50 +0000

    Update Contact Manager
    Further defining the sections for the contact manager code and starting the code for them

--- ContactManager.asm
+++ ContactManager.asm
@@ -3,12 +3,48 @@ section .text
 
 _start:
     call _askforfunction
+    call _listfunctions
+    call _getresponse
 
 _askforfunction:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, querymessage
+    mov rdx, lenquerymessage
+    syscall
+
+    ret
+
+_listfunctions:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, firstoption
+    mov rdx, lenfirstoption
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, secondoption
+    mov rdx, lensecondoption
+    syscall
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, listcontacts 
+    mov rdx, lenlistcontacts
+    syscall
 
     ret
 
 _getresponse:
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, answer
+    mov rdx, 1
+
+    ret
+
+_listcontacts:
 
 _getname:
 
@@ -29,5 +65,17 @@ _closefile:
 _writetofile:
 
 section .data
+    querymessage db 'What functionality are you looking for?', 10
+    lenquerymessage equ $-querymessage
 
-section .text
+    firstoption db 'Enter 1 to Search Contact Book', 10
+    lenfirstoption equ $-firstoption
+
+    secondoption db 'Enter 2 to Insert Contact', 10
+    lensecondoption equ $-secondoption
+
+    listcontacts db 'Enter 3 to List all contacts', 10
+    lenlistcontacts equ $-listcontacts
+
+segment .bss
+    answer resb 1
