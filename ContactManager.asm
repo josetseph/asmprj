10445ca5c9263d2198ff3a05f9eb3808e36d6c98
wrtnspknbrkn <55252590+Joseph-DKE@users.noreply.github.com>
2021-07-05 14:07:16 +0000

    First commit.
    Entire project file.
    Creating file and providing sections.

---
+++ ContactManager.asm
@@ -0,0 +1,33 @@
+section .text
+    global _start
+
+_start:
+    call _askforfunction
+
+_askforfunction:
+
+    ret
+
+_getresponse:
+
+_getname:
+
+_getnum:
+
+_saveinput:
+
+_getfile:
+
+_openfile:
+
+_readfile:
+
+_searchfile:
+
+_closefile:
+
+_writetofile:
+
+section .data
+
+section .text
