87b5e90fea5ccd840a6fec7b98b27a4eb9552e84
wrtnspknbrkn <55252590+Joseph-DKE@users.noreply.github.com>
2021-07-08 03:32:31 +0000

    80% Version of Contact Manager
    Yet to implement a searching algorithm to find contacts already in the contact book.
    Comments to be removed and code to be properly commented for easy understanding.

--- ContactManager.asm
+++ ContactManager.asm
@@ -1,7 +1,11 @@
-O_RDONLY equ 0
-O_WRONLY equ 1
+;Remove all comments
+
+;O_RDONLY    equ 0
+;O_APPEND    equ 1024
+;O_WRONLY    equ 1
 
 section .text
+
 %macro input 2
     mov rax, 0
     mov rdi, 0
@@ -9,26 +13,14 @@ section .text
     mov rdx, %2
     syscall
 %endmacro
-%macro output 3
-    mov rax, 1
-    mov rdi, %1
-    mov rsi, %2
-    mov rdx, %3
-    syscall
-%endmacro
-%macro fileopen 2
-    mov rax, 2
-    mov rdi, %1
-    mov rsi, %2
-    mov rdx, 0777o
-    syscall
-%endmacro
-%macro fileclose 1
-    mov rax, 3
-    mov rdi, %1
-    syscall
-%endmacro
 
+%macro print 2
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, %1
+    mov rdx, %2
+    syscall
+%endmacro
 
     global _start
 
@@ -37,141 +29,237 @@ _start:
     call _listfunctions
     call _getresponse
     call _checkresponse
-    
+
+    jmp _exit
+
 _exit:
     mov rax, 60
     syscall
 
-
 _askforfunction:
-    output 1, querymessage, lenquerymessage
+    print querymessage, lenquerymessage
+
     ret
 
 _listfunctions:
-    output 1, firstoption, lenfirstoption
-    output 1, secondoption, lensecondoption
-    output 1, listcontacts, lenlistcontacts
+    print firstoption, lenfirstoption
+
+    print secondoption, lensecondoption
+
+    print listcontacts, lenlistcontacts
+
     ret
 
 _getresponse:
     input answer, 1
+
     ret
 
 _checkresponse:
-    mov rsi, [answer]
-    sub rsi, '0'
-    mov rbx, '1'
-    sub rbx, '0'
+    mov rcx, [answer]
+    sub rcx, 0x30
 
-    cmp rsi, rbx
+    mov rbx, '1'
+    sub rbx, 0x30
+    cmp rcx, rbx
     je _searchfile
 
-    ;input option
-    mov rbx, 2
-    cmp rsi, rbx
+    mov rbx, '2'
+    sub rbx, 0x30
+    cmp rcx, rbx
     je _getinfo
 
-    ;output list option
-    mov rbx, 3
-    cmp rsi, rbx
+    mov rbx, '3'
+    sub rbx, 0x30
+    cmp rcx, rbx
     je _listcontacts
 
-    call _invalidinput
+    jmp _invalidinput
+
     ret
 
 _invalidinput:
-    output 1, invalidmessage, leninvalidmessage
-    ret
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, invalidmessage
+    mov rdx, leninvalidmessage
+    syscall
+
+    jmp _exit
 
 _listcontacts:
     call _openfileforreading
     call _readfile
     call _closefilereadonly
 
-    output 1, datalog, 1000
-    ret
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, dataloge
+    mov rdx, 1000
+    syscall
+
+    jmp _exit
 
 _getinfo:
-    ;get name
-    output 1, namequestion, lennamequestion
-    input name, 50
-    
-    ;get number
-    output 1, numberquestion, lennumberquestion
-    input number, 50
+    print getinfoprompt, lengetinfoprompt
 
-    ;open file for writing
-    fileopen [filename], O_WRONLY
-    mov [fd_out], rax
-    
+    input name, 75
+    input name, 75
 
-    output [fd_out], name, 50
-    output [fd_out], number, 50
-    fileclose [fd_out]   
-    ret
+    call _savetofile
+
+    jmp _exit
+
+;_countlength:
+    ;mov rax, name
+    ;push rax
+    ;mov rbx, 0
+    ;_loop:
+        ;inc rax
+        ;inc rbx
+	;mov cl, [rax]
+	;cmp cl, 0
+	;jne _loop
+
+    ;ret
+
+_savetofile:
+    ;call _openfileforreading
+    ;call _readfile
+    ;call _closefilereadonly
+
+    ;call _countlength
+
+    call _openfileforwriting
+
+    ;call _countlength
+
+    ;mov rsi, dataloge
+    ;mov rdx, rbx
+    ;mov rdi, [fd_out]
+    ;mov rax, 1
+    ;syscall
+
+    mov rax, name
+    call _writetofile
+
+    ;pop rsi
+    ;mov rdx, rbx
+    ;mov rdi, [fd_out]
+    ;mov rax, 1
+    ;syscall
+
+    ;call _writetofile
+
+    ;mov rsi, endchar
+    ;mov rdx, lenendchar
+    ;mov rdi, [fd_out]
+    ;mov rax, 1
+    ;syscall
+
+    call _closefilewriteonly
+
+    jmp _exit
 
 _openfileforreading:
-    fileopen filename, O_RDONLY    
+    mov rax, 2
+    mov rdi, filename
+    mov rsi, 0x0
+    mov rdx, 0777o
+    syscall
+
     mov [fd_in], rax
-    
+
     ret
-    
+
 _openfileforwriting:
-    fileopen filename, O_WRONLY
+    mov rax, 2
+    mov rdi, filename
+    mov rsi, 0x441
+    mov rdx, 0777o
+    syscall
+
     mov [fd_out], rax
 
     ret
 
 _readfile:
-    input datalog, 1000
+    mov rax, 0
+    mov rdi, [fd_in]
+    mov rsi, dataloge
+    ;call _countlength
+    mov rdx, 1000
+    syscall
+
     ret
 
 _searchfile:
     call _openfileforreading
     call _readfile
     call _closefilereadonly
-    ret
+
+    jmp _exit
 
 _closefilereadonly:
-    fileclose [fd_in]
+    mov rax, 3
+    mov rdi, [fd_in]
+    syscall
+
     ret
-    
+
+_closefilewriteonly:
+    mov rax, 3
+    mov rdi, [fd_out]
+    syscall
+
+    ret
+
+_writetofile:
+    push rax
+    mov rbx, 0
+
+    _printloop:
+	inc rax
+	inc rbx
+	mov cl, [rax]
+	cmp cl, 0
+	jne _printloop
+
+	mov rax, 1
+	mov rdi, [fd_out]
+	mov rdx, rbx
+	pop rsi
+	syscall
+
+    ret
 
 section .data
     querymessage db 'What functionality are you looking for?', 10
     lenquerymessage equ $-querymessage
 
-    firstoption db 'Enter 1 to Search Contact Book', 10
+    firstoption db 'Enter 1 to Search Contact Book.', 10
     lenfirstoption equ $-firstoption
 
-    secondoption db 'Enter 2 to Insert Contact', 10
+    secondoption db 'Enter 2 to Insert Contact.', 10
     lensecondoption equ $-secondoption
 
-    listcontacts db 'Enter 3 to List all contacts', 10
+    listcontacts db 'Enter 3 to List all contacts.', 10
     lenlistcontacts equ $-listcontacts
+
+    getinfoprompt db 'Type name, a space, then the number.', 10
+    lengetinfoprompt equ $-getinfoprompt
 
     invalidmessage db 'Your input is invalid', 10
     leninvalidmessage equ $-invalidmessage
 
-    namequestion db 'Enter your name: '
-    lennamequestion equ $-namequestion
-
-    numberquestion db 'Enter your number: '
-    lennumberquestion equ $-numberquestion
-
     filename db 'contacts.txt', 0
-    lenfilename equ $-filename
 
-    filename db 'contacts.txt'
+    endchar db '', 10, 0
+    lenendchar equ $-endchar
 
 segment .bss
-    answer resb 2
-    
-    name resq 50
-    number resb 15
-    
-    datalog resb 1000
-    
+    answer resb 1
+    name resb 75
     fd_in resb 1
     fd_out resb 1
-
+    dataloge rest 1000
